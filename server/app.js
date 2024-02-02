59611f9cf3ee8fd92571415dcf45026ff4510616
Zehan Khan <zkhan@hootboard.com>
2024-02-03 04:29:36 +0530

    cors fix

--- server/app.js
+++ server/app.js
@@ -25,6 +25,14 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*',cors());
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();  
+}
+app.use(allowCrossDomain);
 
 app.use(cookieParser("secretcode"));
 
